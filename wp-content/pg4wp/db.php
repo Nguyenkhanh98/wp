1b476a0637f1819610b13836126ed738d00c6aaf
NGUYEN Khanh <knguyen@fr.scc.com>
2021-01-23 16:27:02 +0700

    'update

--- wp-content/pg4wp/db.php
+++ wp-content/pg4wp/db.php
@@ -3,7 +3,7 @@
 Plugin Name: PostgreSQL for WordPress (PG4WP)
 Plugin URI: http://www.hawkix.net
 Description: PG4WP is a special 'plugin' enabling WordPress to use a PostgreSQL database.
-Version: 1.3.1
+Version: 1.3.1+
 Author: Hawk__
 Author URI: http://www.hawkix.net
 License: GPLv2 or newer.
@@ -18,18 +18,22 @@ define('DB_DRIVER', 'pgsql'); // 'pgsql' or 'mysql' are supported for now
 define( 'PG4WP_DEBUG', false);
 // If you just want to log queries that generate errors, leave PG4WP_DEBUG to "false"
 // and set this to true
-define( 'PG4WP_LOG_ERRORS', false);
+define( 'PG4WP_LOG_ERRORS', true);
 
 // If you want to allow insecure configuration (from the author point of view) to work with PG4WP,
 // change this to true
 define( 'PG4WP_INSECURE', false);
 
 // This defines the directory where PG4WP files are loaded from
-//   2 places checked : wp-content and wp-content/plugins
+//   3 places checked : wp-content, wp-content/plugins and the base directory
 if( file_exists( ABSPATH.'/wp-content/pg4wp'))
 	define( 'PG4WP_ROOT', ABSPATH.'/wp-content/pg4wp');
-else
+else if( file_exists( ABSPATH.'/wp-content/plugins/pg4wp'))
 	define( 'PG4WP_ROOT', ABSPATH.'/wp-content/plugins/pg4wp');
+else if( file_exists( ABSPATH.'/pg4wp'))
+	define( 'PG4WP_ROOT', ABSPATH.'/pg4wp');
+else
+	die('PG4WP file directory not found');
 
 // Here happens all the magic
 require_once( PG4WP_ROOT.'/core.php');
